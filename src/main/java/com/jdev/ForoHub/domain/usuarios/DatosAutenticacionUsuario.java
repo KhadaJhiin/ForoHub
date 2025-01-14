dd7cd57c0f4dda05c76492b3d5776d7c3c9900be
KhadaJhiin <josemiguelortegagaviria@gmail.com>
2025-01-13 22:46:03 -0500

    Se realiza token

---
+++ src/main/java/com/jdev/ForoHub/domain/usuarios/DatosAutenticacionUsuario.java
@@ -0,0 +1,4 @@
+package com.jdev.ForoHub.domain.usuarios;
+
+public record DatosAutenticacionUsuario(String login, String clave) {
+}
--- src/main/java/com/jdev/ForoHub/infra/security/SecurityConfigurations.java
+++ src/main/java/com/jdev/ForoHub/infra/security/SecurityConfigurations.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -25,17 +27,12 @@ public class SecurityConfigurations {
 //                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 }
 
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        return http.csrf(AbstractHttpConfigurer::disable)
-//                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//                .authorizeHttpRequests(req -> {
-//                    req.requestMatchers(HttpMethod.POST, "/login").permitAll();
-//                    req.anyRequest().authenticated();
-//                })
-//                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
-//                .build();
-//    }
+
 
